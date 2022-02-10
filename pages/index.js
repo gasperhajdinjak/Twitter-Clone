d1dfd04e63af3d4fe38069f2a8d54f07a9d661e5
Saucey999 <hajdinjak.gasper@gmail.com>
2022-02-10 16:00:48 +0100

    hhhh

--- pages/index.js
+++ pages/index.js
@@ -40,8 +40,8 @@ export default function Home({ trendingResults, followResults, providers }) {
 export async function getServerSideProps(context) {
   const trendingResults = await fetch('https://jsonkeeper.com/b/GSZJ', {
     headers: {
-      Accept: 'application/json',
       'Content-Type': 'application/json',
+      Accept: 'application/json',
     },
   }).then(res => res.json());
   const followResults = await fetch('https://jsonkeeper.com/b/WWMJ').then(res =>
